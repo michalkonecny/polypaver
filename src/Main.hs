295903fcf6feba4d8ae4aab18875c15ad03448a5
Jan Duracz <jan@duracz.net>
2011-07-25 21:50:18 +0000

    git-svn-id: https://virgo.aston.ac.uk/FPbyContract/paver@1122 9cb10eec-f628-0410-908a-98ac1d910071

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,18 @@
+{-|
+    The executable.  Dispatch to one of several main functions
+    depending on the first command-line parameter.
+-}
 module Main where
 
-main::IO()
-main = putStrLn "mi"
+import qualified Main.Prover
+import qualified Main.Plotter
+
+import System.Environment
+
+main =
+    do
+    args <- getArgs
+    case args of
+        ("prove" : _) -> Main.Prover.main
+        ("plot" : _) -> Main.Plotter.main
+        _ -> putStrLn "run with one of the following parameters: prove plot"
