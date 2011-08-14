ee53e3f69d9e193fd4ea86884169930a837a3be1
Michal Konečný <mikkonecny@gmail.com>
2011-08-14 16:07:48 +0000

    avoid having to pass arguments repeatedly
    git-svn-id: https://virgo.aston.ac.uk/FPbyContract/paver@1145 9cb10eec-f628-0410-908a-98ac1d910071

--- src/Eval.hs
+++ src/Eval.hs
@@ -11,145 +11,145 @@ import qualified Numeric.ER.BasicTypes.DomainBox as DBox
 import qualified Numeric.ER.Real.Approx.Elementary as RAEL
 
 evalForm maxdeg ix box prec form =
-  case form of
-      Not arg ->
-          L.not $ evalForm maxdeg ix box prec arg 
-      Or left right ->
-          evalForm maxdeg ix box prec left L.|| evalForm maxdeg ix box prec right
-      And left right ->
-          evalForm maxdeg ix box prec left L.&& evalForm maxdeg ix box prec right
-      Implies left right ->
-          evalForm maxdeg ix box prec left L.~> evalForm maxdeg ix box prec right
-      Le left right ->
-          L.not $
-          evalTerm maxdeg ix box prec right `L.leq` evalTerm maxdeg ix box prec left
-      Leq left right ->
-          evalTerm maxdeg ix box prec left `L.leq` evalTerm maxdeg ix box prec right          
-      Ge left right ->
-          L.not $
-          evalTerm maxdeg ix box prec left `L.leq` evalTerm maxdeg ix box prec right
-      Geq left right ->
-          evalTerm maxdeg ix box prec right `L.leq` evalTerm maxdeg ix box prec left
-      Eq left right ->
-          (evalTerm maxdeg ix box prec left `L.leq` evalTerm maxdeg ix box prec right)
-          L.&&
-          (evalTerm maxdeg ix box prec right `L.leq` evalTerm maxdeg ix box prec left)
-      Neq left right ->
-          L.not $ 
-          (evalTerm maxdeg ix box prec left `L.leq` evalTerm maxdeg ix box prec right)
-          L.&&
-          (evalTerm maxdeg ix box prec right `L.leq` evalTerm maxdeg ix box prec left)
-      Ni left right -> 
-          if RA.isBottom rightArg || RA.isBottom leftArg then
-              L.bot
-          else 
-              rightArg `L.includes` leftArg
-          where
-          rightArg = 
-              evalTerm maxdeg ix box prec right
-          leftArg = 
-              evalTerm maxdeg ix box prec left
-        
-evalTerm maxdeg ix box prec term =
---  let size = maxdeg * DBox.size box in 
-  case term of
-      EpsAbs ->
-          FA.setMaxDegree maxdeg $
---          FA.setMaxSize size $
-          fromRational $
---            2^^(-14) -- 16-bit
-            2^^(-126) -- 32-bit
---            2^^(-1022) -- 64-bit
-      EpsRel ->
-          FA.setMaxDegree maxdeg $
---          FA.setMaxSize size $
-          fromRational $
---            2^^(-9) -- 16-bit
-          2^^(1-prec) -- custom float
---            2^^(-22)-- 32-bit
---            2^^(-51) -- 64-bit
-      Pi ->
-          FA.setMaxDegree maxdeg $
---          FA.setMaxSize size $
-          RAEL.pi 10
-      Lit val -> 
-          FA.setMaxDegree maxdeg $
---          FA.setMaxSize size $
-          fromRational $
-          val :: FAPDOI BM
-      Var varid ->
-          case RA.isExact vardom of
-              True -> -- domain of var thin, so var is a const
-                  FA.setMaxDegree maxdeg $ 
---                  FA.setMaxSize size $
-                  FA.const 
-                      DBox.noinfo 
-                      [vardom]
-              False -> -- domain of var not thin, so safe to proj
-                  FA.setMaxDegree maxdeg $
---                  FA.setMaxSize size $ 
-                  FA.proj
-                      (DBox.singleton varid vardom)
-                      varid
-              where                
-              vardom = DBox.lookup "" varid box
-      Plus left right ->
-          evalTerm maxdeg ix box prec left + evalTerm maxdeg ix box prec right
-      Minus left right ->
-          evalTerm maxdeg ix box prec left - evalTerm maxdeg ix box prec right
-      Neg arg ->
-          - evalTerm maxdeg ix box prec arg
-      Abs arg ->
-        let argEncl = evalTerm maxdeg ix box prec arg in 
-          case RA.leqReals 0 argEncl of
-            Just True -> -- argument certainly non-negative
-              argEncl    -- so do nothing
-            _ -> -- otherwise
-              RAEL.sqrt ix $ argEncl^2 -- do smooth approx of abs           
-      Times left right ->
-          evalTerm maxdeg ix box prec left * evalTerm maxdeg ix box prec right
-      Square arg ->
-          evalTerm maxdeg ix box prec arg
-      Recip arg ->
-          recip $ evalTerm maxdeg ix box prec arg
-      Over left right ->
-          evalTerm maxdeg ix box prec left / evalTerm maxdeg ix box prec right
-      Sqrt arg ->
-          RAEL.sqrt (fromInteger $ toInteger ix) $
-            evalTerm maxdeg ix box prec arg
-      Exp arg ->
-          RAEL.exp 
-                ix $ -- (fromInteger $ 3*(toInteger maxdeg)+10) $ 
-                evalTerm maxdeg ix box prec arg                    
-      Hull left right ->
-          evalTerm maxdeg ix box prec left RA.\/ evalTerm maxdeg ix box prec right
-      EpsiAbs ->
-          evalTerm maxdeg ix box prec $
-          (-EpsAbs) `Hull` EpsAbs
-      EpsiRel ->
-          evalTerm maxdeg ix box prec $
-          (-EpsRel) `Hull` EpsRel
-      Round arg ->
-          evalTerm maxdeg ix box prec $
-          ((1 + EpsiRel) * arg) + EpsiAbs 
-      FPlus left right ->
-          evalTerm maxdeg ix box prec $
-          Round (left + right)
-      FMinus left right ->
-          evalTerm maxdeg ix box prec $
-          Round (left - right)        
-      FTimes left right ->
-          evalTerm maxdeg ix box prec $
-          Round (left * right)
-      FSquare arg ->
-          evalTerm maxdeg ix box prec $
-          Round (Square arg)
-      FSqrt arg ->
-          evalTerm maxdeg ix box prec $
-          Round $ (1+2*EpsiRel) * (Sqrt arg)
-      FOver left right ->
-          evalTerm maxdeg ix box prec $
-          Round (left / right)
-      FExp arg ->
-          evalTerm maxdeg ix box prec $
-          Round $ (1+4*EpsiRel) * (Exp arg)
+    evForm form
+    where
+    evForm form =
+        case form of
+          Not arg ->
+              L.not $ evForm arg
+          Or left right ->
+              evForm left L.|| evForm right
+          And left right ->
+              evForm left L.&& evForm right
+          Implies left right ->
+              evForm left L.~> evForm right
+          Le left right ->
+              L.not $
+              evTerm right `L.leq` evTerm left
+          Leq left right ->
+              evTerm left `L.leq` evTerm right          
+          Ge left right ->
+              L.not $
+              evTerm left `L.leq` evTerm right
+          Geq left right ->
+              evTerm right `L.leq` evTerm left
+          Eq left right ->
+              (evTerm left `L.leq` evTerm right)
+              L.&&
+              (evTerm right `L.leq` evTerm left)
+          Neq left right ->
+              L.not $ 
+              (evTerm left `L.leq` evTerm right)
+              L.&&
+              (evTerm right `L.leq` evTerm left)
+          Ni left right -> 
+              if RA.isBottom rightArg || RA.isBottom leftArg then
+                  L.bot
+              else 
+                  rightArg `L.includes` leftArg
+              where
+              rightArg = evTerm right
+              leftArg = evTerm left
+    evTerm term =
+    --  let size = maxdeg * DBox.size box in 
+      case term of
+          EpsAbs ->
+              FA.setMaxDegree maxdeg $
+    --          FA.setMaxSize size $
+              fromRational $
+    --            2^^(-14) -- 16-bit
+                2^^(-126) -- 32-bit
+    --            2^^(-1022) -- 64-bit
+          EpsRel ->
+              FA.setMaxDegree maxdeg $
+    --          FA.setMaxSize size $
+              fromRational $
+    --            2^^(-9) -- 16-bit
+              2^^(1-prec) -- custom float
+    --            2^^(-22)-- 32-bit
+    --            2^^(-51) -- 64-bit
+          Pi ->
+              FA.setMaxDegree maxdeg $
+    --          FA.setMaxSize size $
+              RAEL.pi 10
+          Lit val -> 
+              FA.setMaxDegree maxdeg $
+    --          FA.setMaxSize size $
+              fromRational $
+              val :: FAPDOI BM
+          Var varid ->
+              case RA.isExact vardom of
+                  True -> -- domain of var thin, so var is a const
+                      FA.setMaxDegree maxdeg $ 
+    --                  FA.setMaxSize size $
+                      FA.const 
+                          DBox.noinfo 
+                          [vardom]
+                  False -> -- domain of var not thin, so safe to proj
+                      FA.setMaxDegree maxdeg $
+    --                  FA.setMaxSize size $ 
+                      FA.proj
+                          (DBox.singleton varid vardom)
+                          varid
+                  where                
+                  vardom = DBox.lookup "" varid box
+          Plus left right ->
+              evTerm left + evTerm right
+          Minus left right ->
+              evTerm left - evTerm right
+          Neg arg ->
+              - evTerm arg
+          Abs arg ->
+            let argEncl = evTerm arg in 
+              case RA.leqReals 0 argEncl of
+                Just True -> -- argument certainly non-negative
+                  argEncl    -- so do nothing
+                _ -> -- otherwise
+                  RAEL.sqrt ix $ argEncl^2 -- do smooth approx of abs           
+          Times left right ->
+              evTerm left * evTerm right
+          Square arg ->
+              evTerm arg
+          Recip arg ->
+              recip $ evTerm arg
+          Over left right ->
+              evTerm left / evTerm right
+          Sqrt arg ->
+              RAEL.sqrt (fromInteger $ toInteger ix) $
+                evTerm arg
+          Exp arg ->
+              RAEL.exp 
+                    ix $ -- (fromInteger $ 3*(toInteger maxdeg)+10) $ 
+                    evTerm arg                    
+          Hull left right ->
+              evTerm left RA.\/ evTerm right
+          EpsiAbs ->
+              evTerm $
+              (-EpsAbs) `Hull` EpsAbs
+          EpsiRel ->
+              evTerm $
+              (-EpsRel) `Hull` EpsRel
+          Round arg ->
+              evTerm $
+              ((1 + EpsiRel) * arg) + EpsiAbs 
+          FPlus left right ->
+              evTerm $
+              Round (left + right)
+          FMinus left right ->
+              evTerm $
+              Round (left - right)        
+          FTimes left right ->
+              evTerm $
+              Round (left * right)
+          FSquare arg ->
+              evTerm $
+              Round (Square arg)
+          FSqrt arg ->
+              evTerm $
+              Round $ (1+2*EpsiRel) * (Sqrt arg)
+          FOver left right ->
+              evTerm $
+              Round (left / right)
+          FExp arg ->
+              evTerm $
+              Round $ (1+4*EpsiRel) * (Exp arg)
