e273bd06c9779fc258962cef98390495316b1036
Michal Konečný <mikkonecny@gmail.com>
2011-08-16 15:29:05 +0000

    git-svn-id: https://virgo.aston.ac.uk/FPbyContract/paver@1153 9cb10eec-f628-0410-908a-98ac1d910071

--- src/Main.hs
+++ src/Main.hs
@@ -9,18 +9,26 @@ import Paver
 
 main = 
     defaultMain Problem 
-        {box = [(i, (1,2)) | i <- indices]
+        {box = bench_exp_box
         ,ivars = []
-        ,theorem = bench_sqrt}
+        ,theorem = bench_exp}
+--    defaultMain Problem 
+--        {box = bench_sqrt_box
+--        ,ivars = []
+--        ,theorem = bench_sqrt}
 
 indices = [0..1]
 
 distance = 0.5^^10
 
+bench_sqrt_box = [(i, (1,2)) | i <- indices]
+
 bench_sqrt =
       (Sqrt . product . map Var $ indices)
       `Le`
       (Plus (Lit distance) $ product . map (Sqrt . Var) $ indices)
+
+bench_exp_box = [(i, (0,1)) | i <- indices]
 
 bench_exp =
       (product . map (Exp . Var) $ indices)
