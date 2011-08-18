632ba4f2fcb969de433f403683993929d617b3ee
konecnym <konecnym@9cb10eec-f628-0410-908a-98ac1d910071>
2011-08-18 13:22:31 +0000

    adding polypaver versions of all VCs coming from sparkified versions of proval cosine and sqrt examples
    git-svn-id: https://virgo.aston.ac.uk/FPbyContract/paver@1193 9cb10eec-f628-0410-908a-98ac1d910071

---
+++ src/float_my_cosine2/float_my_cosine2_1.hs
@@ -0,0 +1,15 @@
+module Main(main) where
+
+import Paver
+import Data.Ratio ((%))
+
+main =
+    defaultMain Problem
+        {
+          box = [(0,((-32769) % 1048576,32769 % 1048576))]
+          ,ivars = []
+          ,theorem = thm
+        }
+thm =
+    Implies (Geq (Var 0) (Neg (Over (Lit (32769 % 1)) (Lit (1048576 % 1))))) (Implies (Leq (Var 0) (Over (Lit (32769 % 1)) (Lit (1048576 % 1)))) (Implies (Geq (Var 0) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Implies (Leq (Var 0) (Lit (340282000000000000000000000000000000000 % 1))) (And (Geq (FTimes (Var 0) (Var 0)) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Leq (FTimes (Var 0) (Var 0)) (Lit (340282000000000000000000000000000000000 % 1)))))))
+
