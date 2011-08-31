070ab19d6b0c2ad96d8d7fd8e72fb654abde6dcb
Jan Duracz <jan@duracz.net>
2011-08-31 13:20:15 +0000

    Our sqrt43.
    git-svn-id: https://virgo.aston.ac.uk/FPbyContract/paver@1294 9cb10eec-f628-0410-908a-98ac1d910071

---
+++ src/sqrt43.hs
@@ -0,0 +1,37 @@
+{-|
+    Module      :  Main
+    Description :  scalable properties involving elementary functions 
+    Copyright   :  (c) Jan Duracz, Michal Konecny 
+    License     :  BSD3
+
+    Maintainer  :  jan@duracz.net
+    Stability   :  experimental
+    Portability :  portable
+
+    Scalable properties involving elementary functions.
+-}
+
+module Main 
+(
+  main
+)
+where
+
+import PolyPaver.Paver
+
+main = 
+    defaultMain Problem 
+        {box = b
+        ,theorem = test}
+
+b = [(0, (0.5,2)), (1, (0,3))]
+
+x = Var 0
+
+r = Var 1
+
+test =
+    r |<-| Hull (-x^2/4+x) (x^2/4+1) /\ 
+    Eq r (0.5 *: (r +: x /: r))
+    ---> 
+    0.5 *: (r +: x /: r) |<-| (1+4*EpsiRel) * sqrt(x)
