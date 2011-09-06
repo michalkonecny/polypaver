a6a16a35d3c491393cabc2a8e83cdfb43130d40b
Jan Duracz <jan@duracz.net>
2011-09-06 22:49:56 +0000

    Fixed our sqrt SPARK example to fit the paper.
    git-svn-id: https://virgo.aston.ac.uk/FPbyContract/paver@1405 9cb10eec-f628-0410-908a-98ac1d910071

--- examples/SPARK/sqrt/src/sqrt.adb
+++ examples/SPARK/sqrt/src/sqrt.adb
@@ -5,15 +5,15 @@ procedure sqrt (x : in float; r : out float)
 --# derives r from x;
 --# pre  1.0 <= x and x <= 2.0;
 --# post r in float and
---#      r <= (1.0+9.0*numeric.epsrel)*exact.sqrt(x);
+--#      r <= (1.0+4.0*numeric.epsrel)*exact.sqrt(x);
 is
    s : float;
 begin
    s := x;
-   r := numeric.times(0.5,numeric.plus(x,1.0));
-   while abs(numeric.minus(r,s)) > numeric.epsabs loop
+   r := numeric.plus(numeric.times(0.5,x),0.5);
+   while r /= s loop
+      --# assert r in -0.25*x**2+x .. 0.25*x**2+1.0 ;
       s := r;
-      --# assert 0.5 <= s and s <= 3.0;
       r := numeric.times(0.5,numeric.plus(s,numeric.divide(x,s)));
    end loop;
 end sqrt;
