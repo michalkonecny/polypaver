549156918214c63ec4555b4efa568528a947fc22
konecnym <konecnym@9cb10eec-f628-0410-908a-98ac1d910071>
2013-02-26 10:54:17 +0000

    Added Proval tests that I forgot to add when working on them long time ago.
    git-svn-id: https://virgo.aston.ac.uk/FPbyContract/paver@1476 9cb10eec-f628-0410-908a-98ac1d910071

---
+++ examples/their_sqrt/their_sqrt_15.hs
@@ -0,0 +1,14 @@
+module Main(main) where
+
+import PolyPaver.Paver
+import Data.Ratio ((%))
+
+main =
+    defaultMain Problem
+        {
+          box = [(0,(409018325532672 % 579004697502343,9015995347763200 % 6369051672525773)),(1,(1 % 2,2 % 1))]
+          ,theorem = thm
+        }
+thm =
+    Implies (Geq (Var 0) (Over (Over (Lit (1023 % 1)) (Lit (1024 % 1))) (Sqrt (Var 1)))) (Implies (Leq (Var 0) (Over (Over (Lit (1025 % 1)) (Lit (1024 % 1))) (Sqrt (Var 1)))) (Implies (Geq (Var 1) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Implies (Geq (Var 1) (Over (Lit (1 % 1)) (Lit (2 % 1)))) (Implies (Leq (Var 1) (Lit (2 % 1))) (Implies (Geq (Var 0) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Implies (Leq (Var 0) (Lit (340282000000000000000000000000000000000 % 1))) (Implies (Geq (FTimes (Over (Lit (1 % 1)) (Lit (2 % 1))) (Var 0)) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Implies (Leq (FTimes (Over (Lit (1 % 1)) (Lit (2 % 1))) (Var 0)) (Lit (340282000000000000000000000000000000000 % 1))) (Implies (Geq (FTimes (Var 0) (Var 0)) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Implies (Leq (FTimes (Var 0) (Var 0)) (Lit (340282000000000000000000000000000000000 % 1))) (Implies (Geq (FTimes (FTimes (Var 0) (Var 0)) (Var 1)) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Implies (Leq (FTimes (FTimes (Var 0) (Var 0)) (Var 1)) (Lit (340282000000000000000000000000000000000 % 1))) (Implies (Geq (FMinus (Lit (3 % 1)) (FTimes (FTimes (Var 0) (Var 0)) (Var 1))) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Implies (Leq (FMinus (Lit (3 % 1)) (FTimes (FTimes (Var 0) (Var 0)) (Var 1))) (Lit (340282000000000000000000000000000000000 % 1))) (And (Geq (FTimes (FTimes (Over (Lit (1 % 1)) (Lit (2 % 1))) (Var 0)) (FMinus (Lit (3 % 1)) (FTimes (FTimes (Var 0) (Var 0)) (Var 1)))) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Leq (FTimes (FTimes (Over (Lit (1 % 1)) (Lit (2 % 1))) (Var 0)) (FMinus (Lit (3 % 1)) (FTimes (FTimes (Var 0) (Var 0)) (Var 1)))) (Lit (340282000000000000000000000000000000000 % 1))))))))))))))))))
+
---
+++ examples/their_sqrt/their_sqrt_22.hs
@@ -0,0 +1,14 @@
+module Main(main) where
+
+import PolyPaver.Paver
+import Data.Ratio ((%))
+
+main =
+    defaultMain Problem
+        {
+          box = [(0,(409417757491200 % 579004697502343,529835755569152 % 374650098383869)),(1,(1 % 2,2 % 1))]
+          ,theorem = thm
+        }
+thm =
+    Implies (Geq (Var 0) (Over (Over (Lit (1048575 % 1)) (Lit (1048576 % 1))) (Sqrt (Var 1)))) (Implies (Leq (Var 0) (Over (Over (Lit (1048577 % 1)) (Lit (1048576 % 1))) (Sqrt (Var 1)))) (Implies (Geq (Var 1) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Implies (Geq (Var 1) (Over (Lit (1 % 1)) (Lit (2 % 1)))) (Implies (Leq (Var 1) (Lit (2 % 1))) (Implies (Geq (Var 0) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Implies (Leq (Var 0) (Lit (340282000000000000000000000000000000000 % 1))) (Implies (Geq (FTimes (Over (Lit (1 % 1)) (Lit (2 % 1))) (Var 0)) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Implies (Leq (FTimes (Over (Lit (1 % 1)) (Lit (2 % 1))) (Var 0)) (Lit (340282000000000000000000000000000000000 % 1))) (Implies (Geq (FTimes (Var 0) (Var 0)) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Implies (Leq (FTimes (Var 0) (Var 0)) (Lit (340282000000000000000000000000000000000 % 1))) (Implies (Geq (FTimes (FTimes (Var 0) (Var 0)) (Var 1)) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Implies (Leq (FTimes (FTimes (Var 0) (Var 0)) (Var 1)) (Lit (340282000000000000000000000000000000000 % 1))) (And (Geq (FMinus (Lit (3 % 1)) (FTimes (FTimes (Var 0) (Var 0)) (Var 1))) (Neg (Lit (340282000000000000000000000000000000000 % 1)))) (Leq (FMinus (Lit (3 % 1)) (FTimes (FTimes (Var 0) (Var 0)) (Var 1))) (Lit (340282000000000000000000000000000000000 % 1))))))))))))))))
+
