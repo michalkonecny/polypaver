595cbe5150b634ee53b6c4b6470033cbd1e3df5e
Michal Konečný <mikkonecny@gmail.com>
2013-02-26 10:47:44 +0000

    Disabled building of ProVal examples.
    git-svn-id: https://virgo.aston.ac.uk/FPbyContract/paver@1475 9cb10eec-f628-0410-908a-98ac1d910071

--- examples/mini.hs
+++ examples/mini.hs
@@ -15,7 +15,9 @@ import PolyPaver
 
 main = 
 --    defaultMain problem_exp 
-    defaultMain test_skew 
+--    defaultMain test_skew 
+--    defaultMain sqrt_sin 
+    defaultMain sinsin2
     
 exp_shift =
     Problem box thm 
@@ -36,6 +38,29 @@ test_skew =
         x |<=| y*y + 0.025
 --        exp x |<=| exp (y*y) + 0.1
          
+sqrt_sin =
+    Problem box thm
+    where
+    box = [(0, (0.000001,1))]
+    x = Var 0 "x"
+    thm = 
+        2 * (sqrt(x+1) - 1) |<=| sin(x)
+         
+sinsin =
+    Problem box thm
+    where
+    box = [(0, (0.2,1))]
+    x = Var 0 "x"
+    thm = 
+        sin(3*x+1) |<=| sin(sin(3*x)+1) 
+         
+sinsin2 =
+    Problem box thm
+    where
+    box = [(0, (0.1,0.19))]
+    x = Var 0 "x"
+    thm = 
+        sin(sin(3*x)+1) |<=| sin(3*x+1)
 
     
     
