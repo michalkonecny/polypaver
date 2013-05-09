cc6040121623100786ab7b09c64f952ab23495b9
Jan Duracz <jan@duracz.net>
2013-05-09 20:57:55 +0000

    Updated sqrt example
    git-svn-id: https://virgo.aston.ac.uk/FPbyContract/paver@1738 9cb10eec-f628-0410-908a-98ac1d910071

--- examples/SPARK/sqrt/src/sqrt.adb
+++ examples/SPARK/sqrt/src/sqrt.adb
@@ -1,20 +1,19 @@
 with PolyPaver.Floats;
---# inherit PolyPaver.Exact, PolyPaver.Floats;
---# main_program;
-procedure Sqrt (X : in Float; R : out Float)
---# derives R from X;
---# pre  1.0 <= X and X <= 2.0;
---# post R in Float and
---#      R <= (1.0+4.0*PolyPaver.Floats.Eps_Rel)*PolyPaver.Exact.Sqrt(X);
-is
-	S : Float;
-begin
-	S := x;
-	R := PolyPaver.Floats.Plus(PolyPaver.Floats.Times(0.5,X),0.5);
-	while R /= s loop
-		--# assert R in -0.25*X**2+X .. 0.25*X**2+1.0 ;
-		S := r;
-		R := PolyPaver.Floats.Times(0.5,
-			PolyPaver.Floats.Plus(S,PolyPaver.Floats.Divide(X,S)));
-   end loop;
-end Sqrt;
+package body Example is
+	
+	function Sqrt (X : in Float) return Float
+	is
+		R,S : Float;
+	begin
+		S := X;
+		R := PolyPaver.Floats.Add(PolyPaver.Floats.Multiply(0.5,X),0.5);
+		while R /= s loop
+			--# assert R in -0.25*X**2+X .. 0.25*X**2+1.0 ;
+			S := r;
+			R := PolyPaver.Floats.Multiply(0.5,
+				PolyPaver.Floats.Add(S,PolyPaver.Floats.Divide(X,S)));
+	   end loop;
+	   return R;
+	end Sqrt;
+
+end Example;
