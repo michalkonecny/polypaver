e79857a8930d56ebe0d938f24d66def15876d5d9
Michal Konečný <mikkonecny@gmail.com>
2013-10-03 21:46:24 +0000

    Separated definition of command-line arguments from invocation.  Simplified passing of parameters to the prover loop with the help of the command-line arguments record.
    git-svn-id: https://virgo.aston.ac.uk/FPbyContract/paver@1848 9cb10eec-f628-0410-908a-98ac1d910071

---
+++ src/PolyPaver/Args.hs
@@ -0,0 +1,127 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+
+module PolyPaver.Args where
+
+import System.Console.CmdArgs
+
+import Data.Typeable
+import Data.Data
+import Data.Maybe (catMaybes)
+
+type Args = PolyPaver
+
+data PolyPaver = PolyPaver 
+    {problemId :: [String]
+    ,tightnessValues :: String
+    ,startDegree :: Int -- maximum polynomial degree for the first attempt, -1 means startDegree = degree
+    ,degree :: Int -- maximum polynomial degree to try
+    ,maxSize :: Int  -- maximum number of terms in a polynomial
+    ,effort :: Int  -- effort index for AERN
+    ,minIntegrExp :: Int
+    ,order :: Order
+    ,splitIntFirst :: Bool
+    ,minDepth :: Int -- minimum bisection depth
+    ,maxDepth :: Int -- maximum bisection depth
+    ,maxQueueLength :: Int -- maximum queue length
+    ,time :: Int -- timeout in seconds
+    ,boxSkewing :: Bool
+    ,splitGuessing :: Int
+    ,quiet :: Bool
+    ,verbose :: Bool
+    ,plotWidth :: Int
+    ,plotHeight :: Int
+    }
+    deriving (Show,Data,Typeable)
+
+data Order = 
+    BFS | DFS | DFSthenBFS | BFSFalsifyOnly
+    deriving (Show,Data,Typeable)
+
+paverDefaultArgs :: Args
+paverDefaultArgs =
+    PolyPaver 
+    {problemId = [] &= args &= typ "PROBLEM_ID" 
+    ,tightnessValues = "1" &= name "i"
+        &= groupname "Problem parameters"
+        &= help "value(s) of T to try (if the formula has an unbound variable T) (eg \"2^0..10\" or \"1..10\" or \"1,10,100\") (default = 1)" 
+    ,startDegree = -1 &= name "s" &= help "first polynomial degree to try on each box (default = degree)"
+        &= groupname "Box solving effort"
+    ,degree = 0 &= name "d" &= help "maximum polynomial degree (default = 0)" 
+    ,maxSize = 100 &= name "z" &= help "maximum polynomial term size (default = 100)"
+    ,effort = 10 &= help "for approximating point-wise sqrt and exp (default = 10)" 
+    ,minIntegrExp = 0 &= name "I" &= help "n to compute approximate integration step using 2^(-n)" 
+    ,order = DFSthenBFS 
+        &= groupname "Box subdivision strategy"
+        &= help "sub-problem processing order, bfs for breadth-first or dfs for depth-first, (default = DFSthenBFS)"
+    ,splitIntFirst = False &= name "f" 
+        &= help "whether to split integer valued domains until they are exact before splitting the continuous domains"
+    ,minDepth = 0 &= help "minimum bisection depth (default = 0)"
+    ,maxDepth = 1000 &= name "b" &= help "maximum bisection depth (default = 1000)"
+    ,maxQueueLength = -1 &= name "u" 
+        &= help ("maximum queue size (default = " 
+                    ++ show maxQueueLengthDefaultDFS ++ " for depth-first and "
+                    ++ show maxQueueLengthDefaultBFS ++ " for breadth-first order)")
+    ,time = 7*24*3600 &= help "timeout in seconds (default = 7*24*3600 ie 1 week)"    
+    ,boxSkewing = False &= name "k" &= help "allow parallelepiped boxes, by default only coaxial rectangles" 
+        &= groupname "Experimental"
+    ,splitGuessing = -1 &= name "g" &= opt (20 :: Int) &= help "try guessing the best box splitting direction but do not allow a box in which a pair of box edge lengths exceeds a given ratio (default 20)"
+--    ,epsrelbits = 23 &= name "r" &= help "n to compute machine epsilon using 2^-n (default = 24)" &= groupname "Floating point rounding interpretation in conjectures"
+--    ,epsabsbits = 126 &= name "a" &= help "n to compute denormalised epsilon using 2^-n (default = 126)"
+    ,quiet = False &= help "suppress all output except the final result (default off)" 
+        &= groupname "Verbosity"
+    ,verbose = False &= help "output extra details while paving (default off)"
+    ,plotWidth = 0 &= name "w" &= help "plot width for 2D problems, 0 mean no plotting (default)" 
+        &= groupname "Plotting"
+    ,plotHeight = 0 &= name "h" &= help "plot height for 2D problems, 0 mean no plotting (default)"
+    } 
+    &= help (unlines 
+                ["Tries to decide numerical conjectures (problems) using polynomial enclosures.",
+                 "[PROBLEM_ID] specifies one or more conjectures as follows:                   ",
+                 "  <name>.vc [<conclusion number>]: like a VC in SPARK .siv                   ",
+                 "  <name>.siv [<vc name> [<conclusion number>]]: SPARK-generated VCs          ",
+                 "  <name>.form: using internal syntax (machine generated)                     "
+#ifdef DynamicLoading 
+                 , 
+                 "  <name>.hs [<identifier name>]: Haskell constant of type Problem            "
+#endif
+                 ])
+    &= summary "PolyPaver 0.2 (c) 2011, 2013 Jan Duracz and Michal Konecny (Aston University)"
+    &= name "polypaver"
+
+setDefaults :: PolyPaver -> PolyPaver
+setDefaults = setMaxQLength . setStartDegree
+    where
+    setMaxQLength args =
+        case maxQueueLength args == -1 of
+            False -> args -- maxQueueLength is explicitly set, do no change
+            True -> 
+                case order args of
+                    DFS -> args { maxQueueLength = maxQueueLengthDefaultDFS }
+                    BFS -> args { maxQueueLength = maxQueueLengthDefaultBFS }
+                    DFSthenBFS -> args { maxQueueLength = maxQueueLengthDefaultDFS }
+                    BFSFalsifyOnly -> args { maxQueueLength = maxQueueLengthDefaultBFS }
+    setStartDegree args 
+        | startDegree args == -1 = args { startDegree = degree args }
+        | otherwise = args 
+
+maxQueueLengthDefaultDFS = 50
+maxQueueLengthDefaultBFS = 5000
+
+checkArgs args =
+    catMaybes $ [checkSplitGuessing, checkSkewing]
+    where
+    checkSkewing
+        | boxSkewing args && startDegree args == 0 =
+            Just $
+                 "Box skewing is not compatible with polynomial degree 0."
+                 ++ "\n  Raise starting polynomial degree to a positive value."
+        | otherwise = Nothing
+    checkSplitGuessing 
+        | splitGuessing args /= -1 && startDegree args == 0 =
+            Just $
+                 "Guessing split direction is not compatible with polynomial degree 0."
+                 ++ "\n  Raise starting polynomial degree to a positive value."
+        | otherwise = Nothing
+        
+
--- src/PolyPaver/Invocation.hs
+++ src/PolyPaver/Invocation.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 
 {-|
@@ -23,6 +22,7 @@ module PolyPaver.Invocation
 )
 where
 
+import PolyPaver.Args
 import PolyPaver.PPBox
 import PolyPaver.Form
 import PolyPaver.ProverLoop
@@ -36,11 +36,13 @@ import qualified Numeric.ER.Real.Approx as RA
 import qualified Data.Map as Map
 import qualified Data.IntMap as IMap
 import Data.List (intercalate)
-import Data.Maybe (catMaybes)
 
 import qualified Data.Sequence as Q
+import Data.Typeable
+import Data.Data
+
 import System.Environment (getArgs, getProgName)
-import System.Console.CmdArgs
+import System.Console.CmdArgs (cmdArgs)
 import System.CPUTime
 import System.IO
 
@@ -54,121 +56,11 @@ data Problem = Problem
     }
     deriving (Show,Read,Typeable)
 
-data PolyPaver = PolyPaver 
-    {problemId :: [String]
-    ,tightnessValues :: String
-    ,startDegree :: Int
-    ,degree :: Int
-    ,maxSize :: Int
-    ,effort :: Int
-    ,minIntegrExp :: Int
-    ,order :: Order
-    ,splitIntFirst :: Bool
-    ,minDepth :: Int
-    ,maxDepth :: Int
-    ,maxQueueLength :: Int
-    ,time :: Int
-----    ,epsrelbits :: Int
-----    ,epsabsbits :: Int
-    ,boxSkewing :: Bool
-    ,splitGuessing :: Int
-    ,quiet :: Bool
-    ,verbose :: Bool
-    ,plotWidth :: Int
-    ,plotHeight :: Int
-    }
-    deriving (Show,Data,Typeable)
-
-paver =
-    PolyPaver 
-    {problemId = [] &= args &= typ "PROBLEM_ID" 
-    ,tightnessValues = "1" &= name "i" 
-        &= groupname "Problem parameters"
-        &= help "value(s) of T to try (if the formula has an unbound variable T) (eg \"2^0..10\" or \"1..10\" or \"1,10,100\") (default = 1)" 
-    ,startDegree = -1 &= name "s" &= help "first polynomial degree to try on each box (default = degree)"
-        &= groupname "Box solving effort"
-    ,degree = 0 &= name "d" &= help "maximum polynomial degree (default = 0)" 
-    ,maxSize = 100 &= name "z" &= help "maximum polynomial term size (default = 100)"
-    ,effort = 10 &= help "for approximating point-wise sqrt and exp (default = 10)" 
-    ,minIntegrExp = 0 &= name "I" &= help "n to compute approximate integration step using 2^(-n)" 
-    ,order = DFSthenBFS 
-        &= groupname "Box subdivision strategy"
-        &= help "sub-problem processing order, bfs for breadth-first or dfs for depth-first, (default = DFSthenBFS)"
-    ,splitIntFirst = False &= name "f" 
-        &= help "whether to split integer valued domains until they are exact before splitting the continuous domains"
-    ,minDepth = 0 &= help "minimum bisection depth (default = 0)"
-    ,maxDepth = 1000 &= name "b" &= help "maximum bisection depth (default = 1000)"
-    ,maxQueueLength = -1 &= name "u" 
-        &= help ("maximum queue size (default = " 
-                    ++ show maxQueueLengthDefaultDFS ++ " for depth-first and "
-                    ++ show maxQueueLengthDefaultBFS ++ " for breadth-first order)")
-    ,time = 7*24*3600 &= help "timeout in seconds (default = 7*24*3600 ie 1 week)"    
-    ,boxSkewing = False &= name "k" &= help "allow parallelepiped boxes, by default only coaxial rectangles" 
-        &= groupname "Experimental"
-    ,splitGuessing = -1 &= name "g" &= opt (20 :: Int) &= help "try guessing the best box splitting direction but do not allow a box in which a pair of box edge lengths exceeds a given ratio (default 20)"
---    ,epsrelbits = 23 &= name "r" &= help "n to compute machine epsilon using 2^-n (default = 24)" &= groupname "Floating point rounding interpretation in conjectures"
---    ,epsabsbits = 126 &= name "a" &= help "n to compute denormalised epsilon using 2^-n (default = 126)"
-    ,quiet = False &= help "suppress all output except the final result (default off)" 
-        &= groupname "Verbosity"
-    ,verbose = False &= help "output extra details while paving (default off)"
-    ,plotWidth = 0 &= name "w" &= help "plot width for 2D problems, 0 mean no plotting (default)" 
-        &= groupname "Plotting"
-    ,plotHeight = 0 &= name "h" &= help "plot height for 2D problems, 0 mean no plotting (default)"
-    } 
-    &= help (unlines 
-                ["Tries to decide numerical conjectures (problems) using polynomial enclosures.",
-                 "[PROBLEM_ID] specifies one or more conjectures as follows:                   ",
-                 "  <name>.vc [<conclusion number>]: like a VC in SPARK .siv                   ",
-                 "  <name>.siv [<vc name> [<conclusion number>]]: SPARK-generated VCs          ",
-                 "  <name>.form: using internal syntax (machine generated)                     "
-#ifdef DynamicLoading 
-                 , 
-                 "  <name>.hs [<identifier name>]: Haskell constant of type Problem            "
-#endif
-                 ])
-    &= summary "PolyPaver 0.2 (c) 2011, 2013 Jan Duracz and Michal Konecny (Aston University)"
-    &= name "polypaver"
-
-setDefaults :: PolyPaver -> PolyPaver
-setDefaults = setMaxQLength . setStartDegree
-    where
-    setMaxQLength args =
-        case maxQueueLength args == -1 of
-            False -> args -- maxQueueLength is explicitly set, do no change
-            True -> 
-                case order args of
-                    DFS -> args { maxQueueLength = maxQueueLengthDefaultDFS }
-                    BFS -> args { maxQueueLength = maxQueueLengthDefaultBFS }
-                    DFSthenBFS -> args { maxQueueLength = maxQueueLengthDefaultDFS }
-                    BFSFalsifyOnly -> args { maxQueueLength = maxQueueLengthDefaultBFS }
-    setStartDegree args 
-        | startDegree args == -1 = args { startDegree = degree args }
-        | otherwise = args 
-
-maxQueueLengthDefaultDFS = 50
-maxQueueLengthDefaultBFS = 5000
-
-checkArgs args =
-    catMaybes $ [checkSplitGuessing, checkSkewing]
-    where
-    checkSkewing
-        | boxSkewing args && startDegree args == 0 =
-            Just $
-                 "Box skewing is not compatible with polynomial degree 0."
-                 ++ "\n  Raise starting polynomial degree to a positive value."
-        | otherwise = Nothing
-    checkSplitGuessing 
-        | splitGuessing args /= -1 && startDegree args == 0 =
-            Just $
-                 "Guessing split direction is not compatible with polynomial degree 0."
-                 ++ "\n  Raise starting polynomial degree to a positive value."
-        | otherwise = Nothing
-        
 
 getTightnessValues :: IO [Integer]
 getTightnessValues =
     do
-    argsPre <- cmdArgs paver
+    argsPre <- cmdArgs paverDefaultArgs
     let args = setDefaults argsPre
     return $ parse $ tightnessValues args
     where
@@ -192,7 +84,7 @@ getTightnessValues =
 defaultMain problem = 
     do
     reportCmdLine
-    argsPre <- cmdArgs paver
+    argsPre <- cmdArgs paverDefaultArgs
     let args = setDefaults argsPre
     case checkArgs args of
         [] -> runPaver problem args
@@ -204,7 +96,7 @@ defaultMain problem =
 batchMain problemFactory =
     do
     reportCmdLine
-    argsPre <- cmdArgs paver
+    argsPre <- cmdArgs paverDefaultArgs
     let args = setDefaults argsPre
     case checkArgs args of
         [] -> 
@@ -241,56 +133,18 @@ reportCmdLine
     
 runPaver problem args =
     do
-    initMachineDouble -- round upwards
+    initMachineDouble -- set FPU to round upwards
     hSetBuffering stdout LineBuffering -- print progress in real time, not in batches
-    solveAndReportOnConsole
-        plotSizesOpt
-        plotStepDelayMs
-        ordr -- sub-problem processing order
-        report -- level of verbosity
---        epsrelbitsOpt
---        epsabsbitsOpt 
-        boxSkewingOpt
-        splitGuessingOpt
-        splitIntFirstOpt
-        startdeg -- maximum polynomial degree for the first attempt
-        maxdeg -- maximum polynomial degree to try
-        improvementRatioThreshold -- when to try raising degree/effort and when to give up and split
-        maxsize -- maximum number of terms in a polynomial
-        mindepth -- minimum bisection depth
-        maxdepth -- maximum bisection depth
-        maxQLength -- maximum queue length
-        ix -- effort index for AERN
-        minIntegrationStepSize -- approximate step to use in piecewise numerical integration
-        maxtime -- timeout
-        conj -- formula to be decided, defined in IntegralTest
+    solveAndReportOnConsole args
+        conj -- formula to be decided
         initbox
     where
-    maxdeg = degree args
-    startdeg = case startDegree args of s | s == -1 -> maxdeg; s -> s
-    minIntegrationStepSize = 2^^(- (minIntegrExp args))
-    improvementRatioThreshold = 1.2
-    maxsize = maxSize args 
-    maxtime = toInteger $ time args
-    ix = fromInteger $ toInteger $ effort args
-    mindepth = minDepth args 
-    maxdepth = maxDepth args 
-    maxQLength = maxQueueLength args 
     conj = conjecture problem
     varNames = getFormVarNames conj
     initbox = ppBoxFromIntervals varIsInts varNames boxBounds 
     varIsInts = IMap.fromList $ map (\(var,_,ii) -> (var, ii)) boxBoundsIsInts
     boxBounds = map (\(var,bounds,_) -> (var,bounds)) boxBoundsIsInts
     boxBoundsIsInts = box problem
-    ordr = order args 
     quietOpt = quiet args
     verboseOpt = verbose args
-    report = if quietOpt then ReportNONE else if verboseOpt then ReportALL else ReportNORMAL
---    epsrelbitsOpt = epsrelbits args 
---    epsabsbitsOpt = epsabsbits args 
-    splitGuessingOpt = case splitGuessing args of -1 -> Nothing; n -> Just n
-    splitIntFirstOpt = splitIntFirst args
-    boxSkewingOpt = boxSkewing args
-    plotSizesOpt = (plotWidth args, plotHeight args)
-    plotStepDelayMs = 0
         
