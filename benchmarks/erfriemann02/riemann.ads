70194266c739c8e327efd7288077a2c6cc7383b5
Michal Konečný <mikkonecny@gmail.com>
2013-12-18 10:00:57 +0000

    Added benchmarks that test benefits of substitution on the Riemann integration verification example.  Started an archive of polypaver executables so that we can easily measure its improvement on new benchmark problems.

---
+++ benchmarks/erfriemann02/riemann.ads
@@ -0,0 +1,22 @@
+with PolyPaver.Floats;
+--# inherit PolyPaver.Exact, PolyPaver.Interval, PolyPaver.Integers, PolyPaver.Floats;
+package Riemann is 
+
+function erfRiemann(x : Float; n : Integer) return Float;
+--# pre PolyPaver.Floats.Is_Range(x, 0.0, 4.0)
+--#     and PolyPaver.Integers.Is_Range(n, 1, 100);
+--# return result => 
+--#     PolyPaver.Interval.Contained_In(
+--#         result
+--#         ,
+--#         PolyPaver.Exact.Integral(0.0,x,PolyPaver.Exact.Exp(-PolyPaver.Exact.Integration_Variable**2))
+--#         +
+--#         PolyPaver.Interval.Hull(
+--#              - 0.1*Float(n+1)
+--#              ,
+--#              (1.0-PolyPaver.Exact.Exp(-x**2))*x/Float(n)
+--#              + 0.1*Float(n+1)
+--#         )
+--#     );
+
+end Riemann;
