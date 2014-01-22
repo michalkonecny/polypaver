303f9cd92a4fbad18585153a78a03167fb0f5171
Michal Konečný <mikkonecny@gmail.com>
2014-01-22 11:28:41 +0000

    In the erfRiemann example, let the parameter n be the logarithm of the partition size, allowing to have larger partitions with fewer values of n.  Make the specification much tighter and make the tightness easier to adjust.

--- tools/SPARK2005/examples/erfRiemann/riemann.ads
+++ tools/SPARK2005/examples/erfRiemann/riemann.ads
@@ -1,19 +1,9 @@
 --# inherit PP_Integer, PP_F_Exact, PP_F_Rounded, PP_F_Elementary, PP_LF_Exact, PP_LF_Rounded, PP_LF_Elementary;
 package Riemann is 
 
-   type MyFloat is digits 14;
-
-   -- An approximation of the function e^(-x^2).
-   function exp_Minus_Square(x : MyFloat) return MyFloat;
-   --# pre PP_LF_Exact.Is_Range(Long_Float(x), -1.0, 5.0);
-   --# return result =>
-   --#    PP_LF_Exact.Contained_In
-   --#       (Long_Float(result), 
-   --#        PP_LF_Exact.Exp(Long_Float(-x*x)) + PP_LF_Exact.Interval(-0.000001,0.000001));
-
    -- This constant is a bound for the overall rounding errors introduced
    -- in each segment of the Riemann sum.
-   c : constant Float := 0.001;
+   c : constant Float := 0.000001;
    
    -- An approximation of the function erf(x)*pi/2, which is equal
    -- to the integral \int_0^x(e^(-t^2))dt.  This integral does not
@@ -24,7 +14,7 @@ package Riemann is
    -- Riemann sum equidistant partition.
    function erf_Riemann(x : Float; n : Integer) return Float;
    --# pre PP_F_Exact.Is_Range(x, 0.0, 4.0)
-   --#     and PP_Integer.Is_Range(n, 1, 100);
+   --#     and PP_Integer.Is_Range(n, 1, 10);
    --# return result => 
    --#     PP_F_Exact.Contained_In(
    --#         result
@@ -32,10 +22,10 @@ package Riemann is
    --#         PP_F_Exact.Integral(0.0,x,PP_F_Exact.Exp(-PP_F_Exact.Integration_Variable**2))
    --#         +
    --#         PP_F_Exact.Interval(
-   --#              - c*Float(n+1)
+   --#              - c*Float((2**n)+1)
    --#              ,
-   --#              (1.0-PP_F_Exact.Exp(-x**2))*x/Float(n)
-   --#              + c*Float(n+1)
+   --#              (1.0-PP_F_Exact.Exp(-x**2))*x/Float(2**n)
+   --#              + c*Float((2**n)+1)
    --#         )
    --#     );
 
