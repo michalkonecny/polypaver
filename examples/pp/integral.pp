c424c518978b398e9495b6afbf8b7956eb0b5fa6
Michal Konečný <mikkonecny@gmail.com>
2015-05-26 14:06:45 +0100

    Changed comment style in some pp examples to make it appear nicer in kate.

--- examples/pp/integral.pp
+++ examples/pp/integral.pp
@@ -9,9 +9,9 @@ H1: x <- [0 .. 1].
 H2: y <- [0 .. 1].
 ->
 C1: 
-//    x/80 + // use this line to make the formula false
+/*    x/80 + // use this line to make the formula false */
     Exp(x+y) - Exp(x)
     |<-|
-    [-0.01 .. 0.01] + // comment this line to make the theorem touching
+    [-0.01 .. 0.01] + /* comment this line to make the theorem touching */
     Integral(x, (x + y), Exp(t), t).
     
